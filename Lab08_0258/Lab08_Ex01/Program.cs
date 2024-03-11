f5a1763c5463bd09e2915cfdcb84ad88768c57fc
SitthaKlaphanich <ao2604152014@hotmail.com>
2024-03-11 08:44:52 +0700

    Lab08_Exercise01_result.md

---
+++ Lab08_0258/Lab08_Ex01/Program.cs
@@ -0,0 +1,22 @@
+﻿Student student1 = new Student();
+student1.Name = "Somchai";
+System.Console.WriteLine($"student1 name = {student1.Name}");
+
+var student2 = new Student();
+student2.Name = "Sompong";
+System.Console.WriteLine($"student2 name = {student2.Name}");
+
+class Person
+{
+    private string name = string.Empty;
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+}
+
+class Student:Person
+{
+
+}
---
+++ Lab_result/Lab08_Exercise01_result.md
@@ -0,0 +1,18 @@
+## บันทึกผลที่ได้จากการรันคำสั่งในข้อ 3
+
+![pic](/Pictures/pic-1.png)
+
+- สามารถ Build ได้เพราะ เป็นการสืบทอด Class Inheritance โดยใช้ Class สืบทอดจาก class person
+
+## บันทึกผลที่ได้จากการรันคำสั่งในข้อ 5
+
+![pic](/Pictures/pic-2.png)
+
+- สามารถ Run ได้เพราะ ใช้ class person มาสืบทอด 
+
+## อธิบายสิ่งที่พบในการทดลอง
+
+โปรแกรมจะแสดงผล 
+
+- student1 name = Somchai
+- student2 name = Sompong
