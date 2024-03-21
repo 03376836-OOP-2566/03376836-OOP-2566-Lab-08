858ff085bc9e4c62bb0f8c0a195a2c4e9eaed8df
AnchisaPhetnoi <65030289@kmitl.ac.th>
2024-03-22 03:12:34 +0700

    03376836-OOP-2566-Lab-08

---
+++ 0289/Lab08_Ex03/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+DerivedClass dc = new DerivedClass();
+class BaseClass1
+ {
+    public BaseClass1()
+    {
+        System.Console.WriteLine("This is BaseClass1");
+    }
+ }
+class BaseClass2
+ {
+    public BaseClass2()
+    {
+        System.Console.WriteLine("This is BaseClass2");
+    }
+ }
+
+class DerivedClass : BaseClass1
+{
+    public DerivedClass()
+    {
+        System.Console.WriteLine("This is DerivedClass");
+    }
+}
