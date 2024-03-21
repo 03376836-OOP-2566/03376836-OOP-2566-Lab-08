328d699e9d43eba3b94913ba04c9f8bbef53367b
AnchisaPhetnoi <65030289@kmitl.ac.th>
2024-03-22 02:50:19 +0700

    03376836-OOP-2566-Lab-08

---
+++ 0289/Lab08_Ex01/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Student student1 = new Student();
+student1.Name = "Somchai";
+System.Console.WriteLine($"student1 name = {student1.Name}");
+
+var student2 = new Student();
+student2.Name = "Sompong";
+System.Console.WriteLine($"student2 name = {student2.Name}");
+
+class Person
+{
+    private string name = string.Empty;
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+}
+
+class Student:Person
+{
+
+}
