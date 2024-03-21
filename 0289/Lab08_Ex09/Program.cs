9ee1dfcfdb98117072d09b6bbac00da8044d01ed
AnchisaPhetnoi <65030289@kmitl.ac.th>
2024-03-22 03:17:44 +0700

    03376836-OOP-2566-Lab-08

---
+++ 0289/Lab08_Ex09/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+DerivedClass dc = new DerivedClass();
+dc.PrintBaseHello();
+
+class BaseClass
+{
+    public string Hello = "Hello From BaseClass";
+}
+class DerivedClass : BaseClass
+{
+    new public string Hello = "Hello From DerivedClass";
+
+    public void PrintBaseHello()
+    {
+        string BaseHello = base.Hello;
+        System.Console.WriteLine(BaseHello);
+    }
+}
