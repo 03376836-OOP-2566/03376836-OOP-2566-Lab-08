b35bee6988df109e1f48ebae9107fcf56a92d44e
fulk2014 <144195753+fulk2014@users.noreply.github.com>
2024-03-22 18:15:29 +0700

    65030275-lab08

---
+++ 0275/Lab08_Ex09/Program.cs
@@ -0,0 +1,17 @@
+﻿DerivedClass dc = new DerivedClass();
+dc.PrintBaseHello();
+
+class BaseClass
+{
+    public string Hello = "Hello From BaseClass";
+}
+class DerivedClass : BaseClass
+{
+    new public string Hello = "Hello From DerivedClass";
+
+    public void PrintBaseHello()
+    {
+        string BaseHello = base.Hello;
+        System.Console.WriteLine(BaseHello);
+    }
+}
---
+++ LAB_Resauult/lab08_ex03.md
@@ -0,0 +1,5 @@
+![alt text](image-4.png)
+
+![alt text](image-5.png)
+
+การสร้างคลาส BaseClass1, BaseClass2, และ DerivedClass ใน C# โดย DerivedClass สืบทอดมาจาก BaseClass1
