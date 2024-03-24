ff22f3a22552d5ea66ddae4458d01cb5963eae95
PATTARAPON PONKUL <65030179@kmitl.ac.th>
2024-03-24 14:25:37 +0700

    New commit

--- 65030179_Lab08/Lab08_Ex01/obj/Debug/net8.0/Lab08_Ex01.AssemblyInfo.cs
+++ 65030179_Lab08/Lab08_Ex01/obj/Debug/net8.0/Lab08_Ex01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab08_Ex01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8645aca0f79c3dd955b94d4c057fb1babdf0498a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab08_Ex01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab08_Ex01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
