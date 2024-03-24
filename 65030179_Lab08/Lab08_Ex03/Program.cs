acc45faf5018519f3648e81d943e8bac6fb1ebf5
PATTARAPON PONKUL <65030179@kmitl.ac.th>
2024-03-24 14:43:31 +0700

    commit Ex03

--- 65030179_Lab08/Lab08_Ex03/Program.cs
+++ 65030179_Lab08/Lab08_Ex03/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿DerivedClass dc = new DerivedClass();
+class BaseClass1
+ {
+    public BaseClass1()
+    {
+        System.Console.WriteLine("This is BaseClass1");
+    }
+ }
+class BaseClass2
+ {
+    public BaseClass2()
+    {
+        System.Console.WriteLine("This is BaseClass2");
+    }
+ }
+
+class DerivedClass : BaseClass1
+{
+    public DerivedClass()
+    {
+        System.Console.WriteLine("This is DerivedClass");
+    }
+}
