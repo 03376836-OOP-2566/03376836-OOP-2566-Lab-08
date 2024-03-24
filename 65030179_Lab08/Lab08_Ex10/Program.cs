89a401560a4ca84444f965f48cf361f6be508d35
PATTARAPON PONKUL <65030179@kmitl.ac.th>
2024-03-24 15:26:19 +0700

    commit 10

--- 65030179_Lab08/Lab08_Ex10/Program.cs
+++ 65030179_Lab08/Lab08_Ex10/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿DerivedClass DC = new DerivedClass();
+System.Console.WriteLine($"DC.Field1 = {DC.Field1}");
+BaseClass BC = (BaseClass)DC;
+System.Console.WriteLine($"BC.Field1 = {BC.Field1}");
+
+class BaseClass
+{
+    public string Field1 = "Field1 of BaseClass"; 
+}
+class DerivedClass:BaseClass
+{
+    new public string Field1 = "Field1 of DerivedClass"; 
+}
