5a2d46932c42bf792e49409bd85c00efb713b912
PATTARAPON PONKUL <65030179@kmitl.ac.th>
2024-03-24 15:21:57 +0700

    commit Ex09

--- 65030179_Lab08/Lab08_Ex09/Program.cs
+++ 65030179_Lab08/Lab08_Ex09/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿DerivedClass dc = new DerivedClass();
+dc.PrintBaseHello();
+
+class BaseClass
+{
+    public string Hello = "Hello From BaseClass";
+}
+class DerivedClass : BaseClass
+{
+    new public string Hello = "Hello From DerivedClass";
+
+    public void PrintBaseHello()
+    {
+        string BaseHello = base.Hello;
+        System.Console.WriteLine(BaseHello);
+    }
+}
